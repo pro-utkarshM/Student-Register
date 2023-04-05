cf26f15ccba2c6b02c992c22a4c1ae2b7f9b14e0
pro-utkarshM <projects.utkarshMaurya@gmail.com>
2023-04-05 08:31:36 +0530

    4rth Commit

---
+++ app/src/main/java/com/example/studentregister/StudentViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.studentregister
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.studentregister.db.StudentDao
+
+class StudentViewModelFactory(
+    private val dao: StudentDao
+):ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(StudentViewModel::class.java)){
+            return StudentViewModel(dao) as T
+        }
+        throw IllegalAccessException("Unknown ViewModel Class")
+    }
+}
